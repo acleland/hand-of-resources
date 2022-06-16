c0374d9abce83a93699ea8372a3c122cd5a6a849
Andrea Cleland <andreaclelan@gmail.com>
2022-06-16 09:58:45 -0700

    created ninja_turtle table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS ninja_turtles;
+
+CREATE TABLE ninja_turtles (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR NOT NULL,
+  mask_color VARCHAR,
+  weapon VARCHAR
+);
+
+INSERT INTO ninja_turtles (name, mask_color, weapon) VALUES
+('Leonardo', 'Blue', 'Katana'),
+('Donatello', 'Purple', 'Bō'),
+('Raphael', 'Red', 'Sai'),
+('Michaelangelo', 'Orange','Nunchaku');
