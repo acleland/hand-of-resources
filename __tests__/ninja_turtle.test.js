1fa6322f9424fc08425129f8010254d8f2c7042a
Andrea Cleland <andreaclelan@gmail.com>
2022-06-17 09:41:13 -0700

    implemented POST /turtles route, with test passing

--- __tests__/ninja_turtle.test.js
+++ __tests__/ninja_turtle.test.js
@@ -9,11 +9,21 @@ describe('backend-express-template routes', () => {
   });
   it('should return list of ninja turtles', async () => {
     const res = await request(app).get('/turtles');
+    expect(res.body.length).toEqual(4);
     const leo = res.body.find((turtle) => turtle.id === '1');
     expect(leo).toHaveProperty('name', 'Leonardo');
     expect(leo).toHaveProperty('mask_color', 'Blue');
     expect(leo).toHaveProperty('weapon', 'Katana');
   });
+
+  it('POST /turtles should create a new owner', async () => {
+    const res = await request(app)
+      .post('/turtles')
+      .send({ name: 'Picasso', mask_color: 'Yellow', weapon: 'Paintbrush' });
+    expect(res.status).toBe(200);
+    expect(res.body.name).toBe('Picasso');
+  });
+
   afterAll(() => {
     pool.end();
   });
