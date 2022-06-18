2d9689a71be0dff712dc9d708616fc9e530c8002
Andrea Cleland <andreaclelan@gmail.com>
2022-06-17 21:30:19 -0700

    implemented GET turtles/:id route with test passing

--- __tests__/ninja_turtle.test.js
+++ __tests__/ninja_turtle.test.js
@@ -3,11 +3,26 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const turtles = [
+  {
+    id: '1',
+    name: 'Leonardo',
+    mask_color: 'Blue',
+    weapon: 'Katana',
+  },
+  {
+    id: '2',
+    name: 'Donatello',
+    mask_color: 'Purple',
+    weapon: 'Bō',
+  },
+];
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('should return list of ninja turtles', async () => {
+  it('GET /turtles should return list of ninja turtles', async () => {
     const res = await request(app).get('/turtles');
     expect(res.body.length).toEqual(4);
     const leo = res.body.find((turtle) => turtle.id === '1');
@@ -16,13 +31,25 @@ describe('backend-express-template routes', () => {
     expect(leo).toHaveProperty('weapon', 'Katana');
   });
 
-  it('POST /turtles should create a new owner', async () => {
+  it('GET /turtles/:id should return the details for a given turtle', async () => {
+    const res = await request(app).get('/turtles/2');
+    expect(res.body).toEqual(turtles[1]);
+  });
+
+  it('POST /turtles should create a new ninja turtle', async () => {
     const res = await request(app)
       .post('/turtles')
       .send({ name: 'Picasso', mask_color: 'Yellow', weapon: 'Paintbrush' });
     expect(res.status).toBe(200);
     expect(res.body.name).toBe('Picasso');
   });
+
+  // it('PUT /turtles/:id should update a turtle entry', async () => {
+  //   const res = await request(app)
+  //     .put('/turtles/1')
+  //     .send({ mask_color: 'Pink' });
+  //   expect(res.status).toEqual(200);
+  // });
 
   afterAll(() => {
     pool.end();
